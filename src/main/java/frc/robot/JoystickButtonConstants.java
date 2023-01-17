487b83513b48d618e7b1c8242f833fdfbd65b12e
Gio <gm.giomar@yahoo.com>
2023-01-17 18:01:53 -0500

    Joystick Constants
    Logitech and Xbox C&P from 2022

---
+++ src/main/java/frc/robot/JoystickButtonConstants.java
@@ -0,0 +1,32 @@
+package frc.robot;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants.  This class should not be used for any other purpose.  All constants should be
+ * declared globally (i.e. public static).  Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class JoystickButtonConstants {
+    public static final int kA = 2;
+    public static final int kB = 3;
+    public static final int kY = 4;
+    public static final int kX = 1;
+    
+    public static final int kL1 = 5;
+    public static final int kL2 = 7;
+    public static final int kL3 = 11;
+
+    public static final int kR1 = 6;
+    public static final int kR2 = 8;
+    public static final int kR3 = 12;
+
+    public static final int kBack = 9;
+    public static final int kStart = 10;
+
+    public static final int kPOV_UP = 0; 
+    public static final int kPOV_RIGHT = 90; 
+    public static final int kPOV_DOWN = 180; 
+    public static final int kPOV_LEFT = 270; 
+}
