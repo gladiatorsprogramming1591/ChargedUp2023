b19f0bb9141e7dbcdf19492ae8915ca31374841f
DriveStation2 <gladiatorsprogramming1591@gmail.com>
2023-03-16 13:13:18 -0400

    Cone and Cube Score Only Autos

--- src/main/java/frc/robot/commands/PathPlanner/OneConeScoreSolo.java
+++ src/main/java/frc/robot/commands/PathPlanner/OneConeScoreSolo.java
@@ -1,35 +1,30 @@
-package frc.robot.commands.PathPlanner.Column5;
+package frc.robot.commands.PathPlanner;
 
 import com.pathplanner.lib.PathConstraints;
 import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants;
 import frc.robot.commands.armCommands.ArmToPositionWithEnd;
-import frc.robot.commands.driveCommands.AutoLevel;
 import frc.robot.subsystems.DriveSubsystem.DriveSubsystem;
 import frc.robot.subsystems.MainIntakeSubsystem.ArmSubsystem;
 import frc.robot.subsystems.MainIntakeSubsystem.IntakeSubsystem;
 import frc.robot.subsystems.MainIntakeSubsystem.ArmSubsystem.armPositions;
 
-public class OnePieceAuto5 extends SequentialCommandGroup {
+public class OneConeScoreSolo extends SequentialCommandGroup {
 
-    public OnePieceAuto5(DriveSubsystem driveSubsystem, 
+    public OneConeScoreSolo(DriveSubsystem driveSubsystem, 
                         ArmSubsystem armSubsystem,
                         IntakeSubsystem intakeSubsystem
                         ){
 
-        PathPlannerTrajectory m_firstPath = PathPlanner.loadPath("Cube Score 5", 
+        PathPlannerTrajectory m_firstPath = PathPlanner.loadPath("Cone Score 4", 
             new PathConstraints(2, 3));
-            PathPlannerTrajectory m_secondPath = PathPlanner.loadPath("Cube Reverse 5", 
+        PathPlannerTrajectory m_secondPath = PathPlanner.loadPath("Cone Reverse 4", 
             new PathConstraints(2, 1));
-        PathPlannerTrajectory m_thirdPath = PathPlanner.loadPath("Leave Community 5", 
-            new PathConstraints(2, 3));
-
         
         addCommands(
             new InstantCommand(() -> intakeSubsystem.intakeOn(Constants.IntakeConstants.kIntakePickUp), intakeSubsystem),
@@ -38,9 +33,7 @@ public class OnePieceAuto5 extends SequentialCommandGroup {
             new RunCommand(() -> intakeSubsystem.intakeOn(Constants.IntakeConstants.kIntakeReverse), intakeSubsystem).withTimeout(.25),
             driveSubsystem.followTrajectoryCommand(m_secondPath, false),
             new InstantCommand(() -> intakeSubsystem.intakeOff()),
-            new ParallelCommandGroup(new ArmToPositionWithEnd(armSubsystem, armPositions.HOME).withTimeout(2.0)),
-            driveSubsystem.followTrajectoryCommand(m_thirdPath, false),
-            new AutoLevel(driveSubsystem)
+            new ArmToPositionWithEnd(armSubsystem, armPositions.HOME).withTimeout(2.0)
             );
     }
 }
